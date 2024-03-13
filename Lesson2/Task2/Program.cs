97f09dbe42295cc67ae93b3b97162a93c60f1e42
Shiota <bondar-seryy@mail.ru>
2024-03-13 21:26:30 +0800

    add Lessom2:Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+while (i < N)
+{
+    arr[i] = i + 1;
+    //System.Console.WriteLine(arr[i]);
+    //System.Console.WriteLine('');
+    System.Console.WriteLine($"{arr[i]} ");
+    i = i + 1;
+}
