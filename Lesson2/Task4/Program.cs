6eb8f8456dae3991c6ec84ac8a851185d8a5a2bf
Shiota <bondar-seryy@mail.ru>
2024-03-13 21:58:41 +0800

    add for and foreach

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -12,3 +12,23 @@ while (i < n)
     i = i + 1;
 }
 System.Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
