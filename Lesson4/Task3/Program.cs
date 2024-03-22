1f9d7a6ee4d8715e80f5d21a41f12d41c3323d6a
Shiota <bondar-seryy@mail.ru>
2024-03-22 17:00:20 +0800

    add Lesson4:Task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿string GetLettersFromsString (string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e)==true)
+        {
+            letters= letters + e;
+        }
+    }
+    return letters;
+}  
+string str = Console.ReadLine();
+string result = GetLettersFromsString(str);
+System.Console.WriteLine(result);
