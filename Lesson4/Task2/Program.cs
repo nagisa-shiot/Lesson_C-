1d9d48f043e182022451a23b1c3edf6bbeec799e
Shiota <bondar-seryy@mail.ru>
2024-03-22 16:40:08 +0800

    add Lesson4:Task2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿int [,] CreateMatrix (int rowCountm, int columsCount) 
+{
+    int[,] matrix = new int[rowCountm, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1,1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+}
+
+int [,] matrix = CreateMatrix(3,4);
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if(isInteresting(e)==true)
+    {
+        System.Console.WriteLine(e);
+    }
+}
+
+bool isInteresting(int value)
+{
+   int sumOfDigits = GetSumOfDigits(value);
+   if(sumOfDigits%2==0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum =0;
+    while(value>0)
+    {
+        sum=sum + value%10;
+        value=value/10;
+    }
+    return sum;
+}
