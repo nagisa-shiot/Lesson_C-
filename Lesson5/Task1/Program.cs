5e99da957d40efc947a73a12acf21558a91c2aa8
Shiota <bondar-seryy@mail.ru>
2024-03-31 05:10:01 +0800

    add Lesson5:Task1

--- Lesson5/Task1/Program.cs
+++ Lesson5/Task1/Program.cs
@@ -2,9 +2,15 @@
  {  
     if(n == 1  || n == 0)
     {
+        System.Console.WriteLine($"Stop: {n}");
         return 1;
     }
+    System.Console.WriteLine(n);
      return n * Fact(n-1);
  }
 
- Console.Write(Fact(0));
+ Console.Write(Fact(5));
+
+ //F11 шаг с заходом
+ //F10 шаг с обходом
+ //F5 продолжить
